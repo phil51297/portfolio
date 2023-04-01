9707fefd1c94c830498d076d747c85a0cec7852d
phil51297 <b.philippe.97@gmail.com>
2023-04-02 00:11:20 +0200

    Modified spacing between Work experience and Projects page

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -45,7 +45,7 @@ const data = [
 ];
 const Work = () => {
   return (
-    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 '>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>
         Expériences professionnelles
       </h1>
